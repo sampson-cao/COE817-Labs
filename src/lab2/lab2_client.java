a14fd28fcd676755548a4bf2e9ec97dd32b082f3
Sampson Cao <sampsontiet@gmail.com>
2022-02-08 18:26:40 -0500

    started part 2 of lab

--- src/lab2/lab2_client.java
+++ src/lab2/lab2_client.java
@@ -1,9 +1,60 @@
 package lab2;
+import java.net.*;
+import java.io.*;
 
 public class lab2_client {
-
-	public lab2_client() {
-		// TODO Auto-generated constructor stub
+	
+	// initialize socket and input/output streams
+	private Socket socket = null;
+	private BufferedReader input = null;
+	private BufferedWriter output = null;
+	
+	public lab2_client(String address, int port){
+		
+		// define socket based on constructor parameters
+		try {
+			socket = new Socket(address, port);
+			
+			System.out.println("Successfully connected");
+			
+			// takes input from console and sends it to socket
+			input = new BufferedReader(new InputStreamReader(System.in));
+			output = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+			
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		// Input reading, exit to leave program
+		String in = "";
+		while (in != "exit") {
+			try {
+				in = input.readLine();
+				System.out.println("Message Received: " + in);
+				
+				
+			}catch(IOException ioe) {
+				ioe.printStackTrace();
+			}
+			
+			// Insert encryption code here/receive code 
+		}
+		
+		// close connections
+		try {
+			input.close();
+			output.close();
+			socket.close();
+		}catch(IOException ioe) {
+			ioe.printStackTrace();
+		}
+		
+	}
+	
+	public static void main (String args[]) {
+		lab2_client c = new lab2_client("192.168.2.1", 8000);
 	}
 
 }
--- src/lab2/lab2_server.java
+++ src/lab2/lab2_server.java
@@ -1,9 +1,60 @@
 package lab2;
 
-public class lab2_server {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
 
-	public lab2_server() {
-		// TODO Auto-generated constructor stub
+public class lab2_server {
+	// initialize socket and input/output streams
+	private Socket socket = null;
+	private ServerSocket server = null;
+	private BufferedReader input = null;
+	private BufferedWriter output = null;
+	
+	public lab2_server(int port) {
+		
+
+		try {
+			// start server and wait for incoming connection
+			server = new ServerSocket(port);
+			System.out.println("Server started");
+			
+			System.out.println("Waiting for client to connect...");
+			
+			socket = server.accept();
+			System.out.println("Client accepted");
+			
+			input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			
+			String in = "";
+			
+			while (in != "exit") {
+				try
+                {
+                    in = input.readLine();
+                    System.out.println("Received Input: " + in);
+                    
+                }
+                catch(IOException i)
+                {
+                    System.out.println(i);
+                }
+            }
+            System.out.println("Closing connection");
+ 
+            // close connection
+            socket.close();
+            input.close();
+            output.close();
+			
+			
+		}catch(IOException ioe) {
+			ioe.printStackTrace();
+		}
+		
 	}
 
 }
