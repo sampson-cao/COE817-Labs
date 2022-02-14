8a968d6e814473b0781c3acf07da8ed35270935b
Sampson Cao <sampsontiet@gmail.com>
2022-02-14 13:54:11 -0500

    updated streams and stuff

--- src/lab2/lab2_client.java
+++ src/lab2/lab2_client.java
@@ -1,60 +1,67 @@
 package lab2;
+
 import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.io.*;
 
 public class lab2_client {
-	
+
+	private String ida = "INITIATOR A";
+	final private int PACKET_SIZE = 1024;
 	// initialize socket and input/output streams
 	private Socket socket = null;
-	private BufferedReader input = null;
-	private BufferedWriter output = null;
-	
-	public lab2_client(String address, int port){
-		
+	private InputStream input = null;
+	private OutputStream output = null;
+
+	private byte[] buf = new byte[PACKET_SIZE];
+
+	public lab2_client(String address, int port) {
 		// define socket based on constructor parameters
 		try {
 			socket = new Socket(address, port);
-			
+
 			System.out.println("Successfully connected");
-			
+
 			// takes input from console and sends it to socket
-			input = new BufferedReader(new InputStreamReader(System.in));
-			output = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-			
+			input = socket.getInputStream();
+			output = socket.getOutputStream();
+
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		// Input reading, exit to leave program
 		String in = "";
-		while (in != "exit") {
-			try {
-				in = input.readLine();
-				System.out.println("Message Received: " + in);
-				
-				
-			}catch(IOException ioe) {
-				ioe.printStackTrace();
-			}
-			
-			// Insert encryption code here/receive code 
+		in = ida;
+		System.out.println("Message to send: " + in);
+
+		buf = Arrays.copyOf(in.getBytes(), PACKET_SIZE);
+
+		try {
+			output.write(buf);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		
+
+		// Insert encryption code here/receive code
+
 		// close connections
 		try {
 			input.close();
 			output.close();
 			socket.close();
-		}catch(IOException ioe) {
+		} catch (IOException ioe) {
 			ioe.printStackTrace();
 		}
-		
+
 	}
-	
-	public static void main (String args[]) {
-		lab2_client c = new lab2_client("192.168.2.1", 8000);
+
+	public static void main(String args[]) {
+		lab2_client c = new lab2_client("0.0.0.0", 8000);
 	}
 
 }
--- src/lab2/lab2_server.java
+++ src/lab2/lab2_server.java
@@ -2,59 +2,76 @@ package lab2;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class lab2_server {
+
+	final private int PACKET_SIZE = 1024;
+
+	private String ida = "RESPONDER B";
+	private String km = "NETWORK SECURITY";
+	private String ks = "RYERSON";
+	String in = "";
+	int inlen = 0;
+
 	// initialize socket and input/output streams
 	private Socket socket = null;
 	private ServerSocket server = null;
-	private BufferedReader input = null;
-	private BufferedWriter output = null;
-	
+	private InputStream input = null;
+	private OutputStream output = null;
+
+	private byte[] buf = new byte[PACKET_SIZE];
+
 	public lab2_server(int port) {
-		
 
 		try {
 			// start server and wait for incoming connection
 			server = new ServerSocket(port);
 			System.out.println("Server started");
-			
+			System.out.println(server.getInetAddress());
 			System.out.println("Waiting for client to connect...");
-			
+
 			socket = server.accept();
 			System.out.println("Client accepted");
+
+			input = socket.getInputStream();
 			
-			input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			// receive initial packet
+			inlen = input.readNBytes(buf, 0, 1024);
+			in = new String(buf, StandardCharsets.UTF_8);
+			System.out.println("Received Input: " + in);
+			System.out.println("Size: " + inlen + "\n");
 			
-			String in = "";
-			
-			while (in != "exit") {
-				try
-                {
-                    in = input.readLine();
-                    System.out.println("Received Input: " + in);
-                    
-                }
-                catch(IOException i)
-                {
-                    System.out.println(i);
-                }
-            }
-            System.out.println("Closing connection");
- 
-            // close connection
-            socket.close();
-            input.close();
-            output.close();
+			// send response packet
 			
 			
-		}catch(IOException ioe) {
+			System.out.println("Closing connection");
+
+			// close connection
+			socket.close();
+			input.close();
+			output.close();
+
+		} catch (IOException ioe) {
 			ioe.printStackTrace();
 		}
+
+	}
+	
+	public void encrypt(byte[] bytearr) {
 		
+	}
+
+	public static void main(String args[]) {
+		lab2_server s = new lab2_server(8000);
 	}
 
 }
