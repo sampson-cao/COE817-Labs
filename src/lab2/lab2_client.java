6769cf652e31b9ea21b7920e88132e617c884832
Sampson Cao <sampsontiet@gmail.com>
2022-02-15 14:00:08 -0500

    completed lab2

--- src/lab2/lab2_client.java
+++ src/lab2/lab2_client.java
@@ -2,66 +2,149 @@ package lab2;
 
 import java.net.*;
 import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 import java.util.Arrays;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+
 import java.io.*;
 
 public class lab2_client {
-
-	private String ida = "INITIATOR A";
+	
+	// packet size for sending/receiving
 	final private int PACKET_SIZE = 1024;
-	// initialize socket and input/output streams
+	
+	// Input data
+	// client ID
+	private String ida = "INITIATOR A";
+	// master key
+	private String km = "NETWORK SECURITY";
+	// sent key
+	private String ks = null;
+	
+	// Socket and input/output stream declarations
 	private Socket socket = null;
 	private InputStream input = null;
 	private OutputStream output = null;
-
+	
+	// Key generation and cipher objects
+	Cipher cipher = null;
+	SecretKeyFactory skf = null;
+	SecretKey key = null;
+	
+	// Buffer array representing a packet
 	private byte[] buf = new byte[PACKET_SIZE];
-
-	public lab2_client(String address, int port) {
-		// define socket based on constructor parameters
-		try {
-			socket = new Socket(address, port);
-
-			System.out.println("Successfully connected");
-
-			// takes input from console and sends it to socket
-			input = socket.getInputStream();
-			output = socket.getOutputStream();
-
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
+	
+	// Constructor for the client
+	public lab2_client(String address, int port) throws IOException, NoSuchAlgorithmException, NoSuchPaddingException,
+			InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidKeySpecException {
+		
+		// Key and cipher intialization
+		skf = SecretKeyFactory.getInstance("DES");
+		key = skf.generateSecret(new DESKeySpec(km.getBytes()));
+		cipher = Cipher.getInstance("DES");
+		
+		// Step 0: Connect to server
+		socket = new Socket(address, port);
+		System.out.println("Successfully connected to server");
+		input = socket.getInputStream();
+		output = socket.getOutputStream();
+		
+		// Step 1: send client's ID to server
+		System.out.println("Message to send: " + ida);
+		buf = Arrays.copyOf(ida.getBytes(), PACKET_SIZE);
+		output.write(buf);
+		System.out.println("Message sent");
+		
+		// Step 2: Receive response message from server
+		buf = input.readNBytes(PACKET_SIZE);
+		
+		// removes all trailing bytes from padding in the packet
+		for (int i = 0; i < buf.length; i++) {
+			if (buf[i] == 0) {
+				buf = Arrays.copyOf(buf, i);
+				break;
+			}
 		}
+		
+		// Display received message
+		String msg = new String(buf, StandardCharsets.UTF_8);
+		System.out.println("Response received: " + msg);
+		System.out.println("Size: " + buf.length);
+		printBytesAsHex(buf);
+		
+		// decrypt message
+		buf = decrypt(buf);
+		msg = new String(buf, StandardCharsets.UTF_8);
+		
+		// process message and retrieve tokens
+		msg = msg.trim();
+		String[] tokens = msg.split(";");
+		String idb = tokens[1];
+		String ks = tokens[2];
+		System.out.println("Server ID = " + idb + ", Key Received = " + ks);
+		
+		// encrypt outgoing packet with received key
+		key = skf.generateSecret(new DESKeySpec(ks.getBytes()));	
+		buf = Arrays.copyOf(encrypt(idb.getBytes()), PACKET_SIZE);
+		
+		// Step 3: Send message to server
+		output.write(buf);
+		System.out.println("Message Sent");
+		
+		// close resources and connection
+		System.out.println("Closing connection");
+		input.close();
+		output.close();
+		socket.close();
 
-		// Input reading, exit to leave program
-		String in = "";
-		in = ida;
-		System.out.println("Message to send: " + in);
-
-		buf = Arrays.copyOf(in.getBytes(), PACKET_SIZE);
-
-		try {
-			output.write(buf);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		// Insert encryption code here/receive code
-
-		// close connections
-		try {
-			input.close();
-			output.close();
-			socket.close();
-		} catch (IOException ioe) {
-			ioe.printStackTrace();
-		}
+	}
+	// Encrypts the byteArr fed into it as input
+	public byte[] encrypt(byte[] byteArr) throws InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+		byte[] output = null;
+		cipher.init(Cipher.ENCRYPT_MODE, key);
+		output = cipher.doFinal(byteArr);
+		System.out.println("Byte Array encrypted: ");
+		System.out.println(new String(output, StandardCharsets.UTF_8));
+		printBytesAsHex(output);
+		return output;
 
 	}
 
+	// Decrypts the byteArr fed into it as input
+	public byte[] decrypt(byte[] byteArr) throws InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+		byte[] output = null;
+		cipher.init(Cipher.DECRYPT_MODE, key);
+		output = cipher.doFinal(byteArr);
+		System.out.println("Byte Array decrypted: ");
+		System.out.println(new String(output, StandardCharsets.UTF_8));
+		printBytesAsHex(output);
+		return output;
+	}
+
+	// Helper function for printing byte array into hex values
+	public static void printBytesAsHex(byte[] byteArr) {
+		StringBuilder byteArray = new StringBuilder();
+		for (byte b : byteArr) {
+			byteArray.append(String.format("%02X ", b));
+		}
+		System.out.println(byteArray);
+	}
+	
 	public static void main(String args[]) {
-		lab2_client c = new lab2_client("0.0.0.0", 8000);
+		try {
+			lab2_client c = new lab2_client("0.0.0.0", 8000);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
--- src/lab2/lab2_server.java
+++ src/lab2/lab2_server.java
@@ -1,77 +1,154 @@
 package lab2;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Arrays;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
 
 public class lab2_server {
-
+	
+	// packet size for sending/receiving
 	final private int PACKET_SIZE = 1024;
-
-	private String ida = "RESPONDER B";
+	
+	// Input data
+	// Server ID
+	private String idb = "RESPONDER B";
+	// Master key
 	private String km = "NETWORK SECURITY";
+	// Key to send
 	private String ks = "RYERSON";
-	String in = "";
-	int inlen = 0;
+	// String representation of message
+	String msg = "";
 
-	// initialize socket and input/output streams
+	// Declare socket and input/output streams
 	private Socket socket = null;
 	private ServerSocket server = null;
 	private InputStream input = null;
 	private OutputStream output = null;
-
+	
+	// Buffer array used to represent a packet
 	private byte[] buf = new byte[PACKET_SIZE];
+	
+	// Key generation and cipher objects 
+	SecretKeyFactory skf = null;
+	SecretKey key = null;
+	Cipher cipher = null;
 
-	public lab2_server(int port) {
+	public lab2_server(int port) throws NoSuchAlgorithmException, InvalidKeyException, InvalidKeySpecException,
+			NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, IOException {
 
-		try {
-			// start server and wait for incoming connection
-			server = new ServerSocket(port);
-			System.out.println("Server started");
-			System.out.println(server.getInetAddress());
-			System.out.println("Waiting for client to connect...");
-
-			socket = server.accept();
-			System.out.println("Client accepted");
-
-			input = socket.getInputStream();
-			
-			// receive initial packet
-			inlen = input.readNBytes(buf, 0, 1024);
-			in = new String(buf, StandardCharsets.UTF_8);
-			System.out.println("Received Input: " + in);
-			System.out.println("Size: " + inlen + "\n");
-			
-			// send response packet
-			
-			
-			System.out.println("Closing connection");
-
-			// close connection
-			socket.close();
-			input.close();
-			output.close();
-
-		} catch (IOException ioe) {
-			ioe.printStackTrace();
+		// Key setup for encryption
+		skf = SecretKeyFactory.getInstance("DES");
+		key = skf.generateSecret(new DESKeySpec(km.getBytes()));
+		cipher = Cipher.getInstance("DES");
+		System.out.println("Key Generated");
+		// Autokey ks if key is not long enough
+		while (ks.length() < 8) {
+			int i = 0;
+			ks += ks.charAt(i);
+			i++;
 		}
 
-	}
-	
-	public void encrypt(byte[] bytearr) {
+		// Step 0: start server and wait for incoming connection
+		server = new ServerSocket(port);
+		System.out.println("Server started");
+		System.out.println("Server information: " + server.getInetAddress());
+		System.out.println("Waiting for client to connect...");
+		socket = server.accept();
+		System.out.println("Client accepted");
+		input = socket.getInputStream();
+		output = socket.getOutputStream();
+
+		// Step 1: receive ID from client
+		buf = input.readNBytes(PACKET_SIZE);
+		msg = new String(buf, StandardCharsets.UTF_8);
+		System.out.println("Received Input: " + msg);
+		System.out.println("Size: " + msg.length());
+		msg = msg.trim();
 		
+		// Step 2: Send response message containing client ID, server ID, and a different key
+		msg += ";" + idb + ";" + ks;
+		System.out.println("Message to send: " + msg);
+		buf = Arrays.copyOf(encrypt(msg.getBytes()), PACKET_SIZE);
+		output.write(buf);
+		System.out.println("Message sent");
+		
+		// Step 3: Receive message from client
+		buf = input.readNBytes(PACKET_SIZE);
+		System.out.println("Received Input: " + new String(buf, StandardCharsets.UTF_8));
+		System.out.println("Size: " + buf.length);
+		printBytesAsHex(buf);
+		// remove padding bytes
+		for (int i = 0; i < buf.length; i++) {
+			if (buf[i] == 0) {
+				buf = Arrays.copyOf(buf, i);
+			}
+		}
+		
+		// configure key and decrypt message
+		key = skf.generateSecret(new DESKeySpec(ks.getBytes()));
+		buf = decrypt(buf);
+		msg = new String(buf, StandardCharsets.UTF_8);
+
+		// close resources and connection
+		System.out.println("Closing connection");
+		socket.close();
+		input.close();
+		output.close();
+	}
+
+	// Encrypts the byteArr fed into it as input
+	public byte[] encrypt(byte[] byteArr) throws InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+		byte[] output = null;
+		cipher.init(Cipher.ENCRYPT_MODE, key);
+		output = cipher.doFinal(byteArr);
+		System.out.println("Byte Array encrypted: ");
+		System.out.println(new String(output, StandardCharsets.UTF_8));
+		printBytesAsHex(output);
+		return output;
+
+	}
+
+	// Decrypts the byteArr fed into it as input
+	public byte[] decrypt(byte[] byteArr) throws InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+		byte[] output = null;
+		cipher.init(Cipher.DECRYPT_MODE, key);
+		output = cipher.doFinal(byteArr);
+		System.out.println("Byte Array decrypted: ");
+		System.out.println(new String(output, StandardCharsets.UTF_8));
+		printBytesAsHex(output);
+		return output;
+	}
+
+	// Helper function for printing byte array into hex values
+	public static void printBytesAsHex(byte[] byteArr) {
+		StringBuilder byteArray = new StringBuilder();
+		for (byte b : byteArr) {
+			byteArray.append(String.format("%02X ", b));
+		}
+		System.out.println(byteArray);
 	}
 
 	public static void main(String args[]) {
-		lab2_server s = new lab2_server(8000);
+		try {
+			lab2_server s = new lab2_server(8000);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
