5a88e0dd74d06e0ac18ea14af52d78a949173621
Sampson Cao <sampsontiet@gmail.com>
2022-03-04 13:44:13 -0500

    added public key distribution

--- src/lab3/Lab3SecureChatClient.java
+++ src/lab3/Lab3SecureChatClient.java
@@ -9,18 +9,22 @@ import java.net.UnknownHostException;
 import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
 import java.security.Key;
+import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
 import java.security.spec.RSAPublicKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.Arrays;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 
 public class Lab3SecureChatClient {
@@ -30,8 +34,9 @@ public class Lab3SecureChatClient {
 	final static private int PORT = 8000;
 
 	// Key-related objects
-	Cipher cipher = null;
-	
+	Cipher cipherDES = null;
+	Cipher cipherRSA = null;
+
 	PublicKey puKey;
 	PublicKey serverKey;
 	PrivateKey prKey;
@@ -45,12 +50,26 @@ public class Lab3SecureChatClient {
 	// Buffer array representing a packet
 	private byte[] buf = new byte[PACKET_SIZE];
 
+	private byte[] msg = new byte[PACKET_SIZE];
+
 	public Lab3SecureChatClient() {
 		try {
 			generateKey();
 			if (connect()) {
 				// Send public key to server
-				sendMessage(puKey.getEncoded(), false);
+				sendMessage(puKey.getEncoded());
+				printBytesAsHex(puKey.getEncoded());
+
+				// Receive server's public key
+				buf = input.readNBytes(PACKET_SIZE);
+				msg = unpad(buf);
+				serverKey = createPublicKey(msg);
+				System.out.println("Server public key retrieved:");
+				printBytesAsHex(msg);
+				
+				
+				
+
 			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -58,11 +77,22 @@ public class Lab3SecureChatClient {
 		}
 	}
 
+	void initializeCiphers() throws NoSuchAlgorithmException, NoSuchPaddingException {
+		cipherDES = Cipher.getInstance("DES");
+		cipherRSA = Cipher.getInstance("RSA");
+	}
+
 	void generateKey() throws NoSuchAlgorithmException {
 		KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
 		KeyPair kp = kpg.genKeyPair();
 		puKey = kp.getPublic();
 		prKey = kp.getPrivate();
+	}
+
+	PublicKey createPublicKey(byte[] key) throws NoSuchAlgorithmException, InvalidKeySpecException {
+		X509EncodedKeySpec keySpec = new X509EncodedKeySpec(key);
+		KeyFactory kf = KeyFactory.getInstance("RSA");
+		return kf.generatePublic(keySpec);
 	}
 
 	void generateSessionKey() throws NoSuchAlgorithmException {
@@ -86,40 +116,122 @@ public class Lab3SecureChatClient {
 			return false;
 		}
 	}
-	
-	
-	void sendMessage(byte[] msg, boolean encrypt, String algo) throws InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
 
-		if (encrypt) {
-			if (algo.equalsIgnoreCase("DES")) {
-				msg = encryptDES(msg);
+	void sendMessage(byte[] msg) throws IOException {
+		if (msg.length <= PACKET_SIZE) {
+			buf = Arrays.copyOf(msg, PACKET_SIZE);
+			output.write(buf);
+			System.out.println("Wrote to server: ");
+			printBytesAsHex(buf);
+		} else {
+			for (int i = 0; i < msg.length; i++) {
+				// send message then start overwriting buffer
+				if (i % PACKET_SIZE == 0) {
+					output.write(buf);
+					System.out.println("Wrote to server: ");
+					printBytesAsHex(buf);
+				} else {
+					buf[i % PACKET_SIZE] = msg[i];
+				}
 			}
 		}
-
-		buf = Arrays.copyOf(msg, PACKET_SIZE);
-
+		output.flush();
 	}
 
 	// Encrypts the byteArr fed into it as input
-	public byte[] encryptDES(byte[] byteArr) throws InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+	public byte[] encryptDES(byte[] byteArr)
+			throws InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
 		byte[] output = null;
-		cipher.init(Cipher.ENCRYPT_MODE, sessionKey);
-		output = cipher.doFinal(byteArr);
+		cipherDES.init(Cipher.ENCRYPT_MODE, sessionKey);
+
+		System.out.println("Byte Array to encrypt: ");
+		System.out.println("String representation: " + new String(output, StandardCharsets.UTF_8));
+		printBytesAsHex(byteArr);
+
+		output = cipherDES.doFinal(byteArr);
 		System.out.println("Byte Array encrypted: ");
-		System.out.println(new String(output, StandardCharsets.UTF_8));
+		System.out.println("String Representation: " + new String(output, StandardCharsets.UTF_8));
 		printBytesAsHex(output);
 		return output;
 
 	}
 
 	// Decrypts the byteArr fed into it as input
-	public byte[] decryptDES(byte[] byteArr) throws InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+	public byte[] decryptDES(byte[] byteArr)
+			throws InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
 		byte[] output = null;
-		cipher.init(Cipher.DECRYPT_MODE, sessionKey);
-		output = cipher.doFinal(byteArr);
+		cipherDES.init(Cipher.DECRYPT_MODE, sessionKey);
+
+		System.out.println("Byte Array to decrypt: ");
+		System.out.println("String representation: " + new String(output, StandardCharsets.UTF_8));
+		printBytesAsHex(byteArr);
+
+		output = cipherDES.doFinal(byteArr);
 		System.out.println("Byte Array decrypted: ");
-		System.out.println(new String(output, StandardCharsets.UTF_8));
+		System.out.println("String representation: " + new String(output, StandardCharsets.UTF_8));
 		printBytesAsHex(output);
+		return output;
+	}
+
+	// method overloading to make the boolean an optional parameter
+	public byte[] encryptRSA(byte[] byteArr)
+			throws InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+		return encryptRSA(byteArr, true);
+	}
+
+	// encrypts the byteArr fed into it using RSA algorithm
+	public byte[] encryptRSA(byte[] byteArr, boolean pub)
+			throws InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+		byte[] output = null;
+		cipherDES.init(Cipher.ENCRYPT_MODE, puKey);
+
+		System.out.println("Byte Array to encrypt: ");
+		System.out.println("String representation: " + new String(output, StandardCharsets.UTF_8));
+		printBytesAsHex(byteArr);
+
+		output = cipherRSA.doFinal(byteArr);
+		System.out.println("Byte Array encrypted");
+		System.out.println("String representation: " + new String(output, StandardCharsets.UTF_8));
+		printBytesAsHex(output);
+		return output;
+	}
+
+	// method overloading to make the boolean an optional parameter
+	public byte[] decryptRSA(byte[] byteArr)
+			throws InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+		return decryptRSA(byteArr, true);
+	}
+
+	// Decrypts the byteArr fed into it using RSA algorithm
+	public byte[] decryptRSA(byte[] byteArr, boolean priv)
+			throws InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+		byte[] output = null;
+		cipherRSA.init(Cipher.DECRYPT_MODE, prKey);
+
+		System.out.println("Byte Array to decrypt: ");
+		System.out.println("String representation: " + new String(output, StandardCharsets.UTF_8));
+		printBytesAsHex(byteArr);
+
+		output = cipherRSA.doFinal(byteArr);
+		System.out.println("Byte Array decrypted: ");
+		System.out.println("String representation: " + new String(output, StandardCharsets.UTF_8));
+		printBytesAsHex(output);
+		return output;
+	}
+
+	/**
+	 * Unpads input byte array removes the null bytes at the end
+	 * 
+	 * @return Input byte array without null padding bytes
+	 */
+	byte[] unpad(byte[] arr) {
+		byte[] output = null;
+		for (int i = arr.length - 1; i >= 0; i--) {
+			if (arr[i] != 0) {
+				output = Arrays.copyOf(arr, i + 1);
+				break;
+			}
+		}
 		return output;
 	}
 
@@ -130,8 +242,9 @@ public class Lab3SecureChatClient {
 	// Helper function for printing byte array into hex values
 	public static void printBytesAsHex(byte[] byteArr) {
 		StringBuilder byteArray = new StringBuilder();
+		System.out.print("Byte representation: ");
 		for (byte b : byteArr) {
-			byteArray.append(String.format("%02X", b));
+			byteArray.append(String.format("%02X ", b));
 		}
 		System.out.println(byteArray);
 	}
--- src/lab3/Lab3SecureChatServer.java
+++ src/lab3/Lab3SecureChatServer.java
@@ -9,14 +9,19 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketAddress;
 import java.net.UnknownHostException;
+import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
 import java.security.spec.RSAPublicKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Arrays;
 
 import javax.crypto.SecretKey;
+import javax.xml.crypto.dsig.keyinfo.X509IssuerSerial;
 
 public class Lab3SecureChatServer {
 
@@ -37,14 +42,34 @@ public class Lab3SecureChatServer {
 	// Buffer array representing a packet
 	private byte[] buf = new byte[PACKET_SIZE];
 
+	private byte[] msg = null;
+
 	public Lab3SecureChatServer() {
 		try {
 			generateKey();
 			createServer();
 			if (connect()) {
-				// do stuff
+
+				// Retrieve public key from client and send server's public key
+				buf = input.readNBytes(PACKET_SIZE);
+				System.out.println("Received from client: ");
+				printBytesAsHex(buf);
+				msg = unpad(buf);
+				clientKey = createPublicKey(msg);
+				System.out.println("Received client's public key: ");
+				printBytesAsHex(msg);
+				
+				// Send server's public key to client
+				System.out.println("Sending server's public key to client");
+				buf = puKey.getEncoded();
+				sendMessage(buf);
+				
+				
+
 			}
-		} catch (Exception e) {
+		} catch (
+
+		Exception e) {
 			e.printStackTrace();
 		}
 	}
@@ -79,13 +104,53 @@ public class Lab3SecureChatServer {
 			return false;
 		}
 	}
-	
-	SecretKey getSessionKey() {
-		
+
+	PublicKey createPublicKey(byte[] key) throws NoSuchAlgorithmException, InvalidKeySpecException {
+		X509EncodedKeySpec keySpec = new X509EncodedKeySpec(key);
+		KeyFactory kf = KeyFactory.getInstance("RSA");
+		return kf.generatePublic(keySpec);
 	}
-	
-	void sendMessage(byte[] msg, boolean encrypt) {
-		
+
+	SecretKey getSessionKey() {
+		return null;
+	}
+
+	void sendMessage(byte[] msg) throws IOException {
+		if (msg.length <= PACKET_SIZE) {
+			buf = Arrays.copyOf(msg, PACKET_SIZE);
+			output.write(buf);
+			output.flush();
+			System.out.println("Wrote to client: ");
+			printBytesAsHex(buf);
+		} else {
+			for (int i = 0; i < msg.length; i++) {
+				// send message then start overwriting buffer
+				if (i % PACKET_SIZE == 0) {
+					output.write(buf);
+					output.flush();
+					System.out.println("Wrote to client: ");
+					printBytesAsHex(buf);
+				} else {
+					buf[i % PACKET_SIZE] = msg[i];
+				}
+			}
+		}
+	}
+
+	/**
+	 * Unpads input byte array removes the null bytes at the end
+	 * 
+	 * @return Input byte array without null padding bytes
+	 */
+	byte[] unpad(byte[] arr) {
+		byte[] output = null;
+		for (int i = arr.length - 1; i >= 0; i--) {
+			if (arr[i] != 0) {
+				output = Arrays.copyOf(arr, i + 1);
+				break;
+			}
+		}
+		return output;
 	}
 
 	public static void main(String[] args) {
@@ -95,10 +160,11 @@ public class Lab3SecureChatServer {
 	// Helper function for printing byte array into hex values
 	public static void printBytesAsHex(byte[] byteArr) {
 		StringBuilder byteArray = new StringBuilder();
+		System.out.print("Byte representation: ");
 		for (byte b : byteArr) {
 			byteArray.append(String.format("%02X ", b));
 		}
 		System.out.println(byteArray);
 	}
-	
+
 }
